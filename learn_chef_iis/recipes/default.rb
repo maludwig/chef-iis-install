4de551a86c671650dcd8d52421e2217c78aba8af
Mitchell Ludwig <mitchell.ludwig@gmail.com>
2017-03-02 02:33:54 -0700

    Installs all required features

--- learn_chef_iis/recipes/default.rb
+++ learn_chef_iis/recipes/default.rb
@@ -3,8 +3,8 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
-powershell_script 'Install IIS' do
-  code 'Add-WindowsFeature Web-Server'
+powershell_script 'Install Web Server (IIS) and dependencies' do
+  code 'Add-WindowsFeature Web-Server,Web-WebServer,Web-Common-Http,Web-Default-Doc,Web-Dir-Browsing,Web-Http-Errors,Web-Static-Content,Web-Health,Web-Http-Logging,Web-Performance,Web-Stat-Compression,Web-Security,Web-Filtering,Web-App-Dev,Web-Net-Ext45,Web-ASP,Web-Asp-Net45,Web-ISAPI-Ext,Web-ISAPI-Filter,Web-Mgmt-Tools,Web-Mgmt-Console,NET-Framework-45-Features,NET-Framework-45-Core,NET-Framework-45-ASPNET'
   guard_interpreter :powershell_script
   not_if '(Get-WindowsFeature -Name Web-Server).Installed'
 end
@@ -15,4 +15,9 @@ end
 
 template 'c:\inetpub\wwwroot\Default.htm' do # ~FC033
   source 'Default.htm.erb'
+end
+
+directory 'c:\ISSSite' do
+  rights :read, 'IIS_IUSRS'
+  recursive true
 end
